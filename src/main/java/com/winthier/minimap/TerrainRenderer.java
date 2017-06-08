806aa9f65dcb95b5bc4e1eaab789997be90c5850
StarTux <ths.loy@googlemail.com>
2017-06-08 15:11:26 +0200

    Update for 1.12

--- src/main/java/com/winthier/minimap/TerrainRenderer.java
+++ src/main/java/com/winthier/minimap/TerrainRenderer.java
@@ -423,8 +423,8 @@ public final class TerrainRenderer extends MapRenderer {
         case WOOL:
         case STAINED_CLAY:
         case STAINED_GLASS:
-        // case CONCRETE:
-        // case CONCRETE_POWDER:
+        case CONCRETE:
+        case CONCRETE_POWDER:
             switch (block.getData()) {
             case 0: return Colors.WOOL_WHITE + shade;
             case 1: return Colors.WOOL_ORANGE + shade;
@@ -444,22 +444,22 @@ public final class TerrainRenderer extends MapRenderer {
             case 15: return Colors.WOOL_BLACK + shade;
             default: return 0;
             }
-        // case WHITE_GLAZED_TERRACOTTA: return Colors.WOOL_WHITE + shade;
-        // case ORANGE_GLAZED_TERRACOTTA: return Colors.WOOL_ORANGE + shade;
-        // case MAGENTA_GLAZED_TERRACOTTA: return Colors.WOOL_MAGENTA + shade;
-        // case LIGHT_BLUE_GLAZED_TERRACOTTA: return Colors.WOOL_LIGHT_BLUE + shade;
-        // case YELLOW_GLAZED_TERRACOTTA: return Colors.WOOL_YELLOW + shade;
-        // case LIME_GLAZED_TERRACOTTA: return Colors.WOOL_LIME + shade;
-        // case PINK_GLAZED_TERRACOTTA: return Colors.WOOL_PINK + shade;
-        // case GRAY_GLAZED_TERRACOTTA: return Colors.WOOL_GRAY + shade;
-        // case SILVER_GLAZED_TERRACOTTA: return Colors.WOOL_SILVER + shade;
-        // case CYAN_GLAZED_TERRACOTTA: return Colors.WOOL_CYAN + shade;
-        // case PURPLE_GLAZED_TERRACOTTA: return Colors.WOOL_PURPLE + shade;
-        // case BLUE_GLAZED_TERRACOTTA: return Colors.WOOL_BLUE + shade;
-        // case BROWN_GLAZED_TERRACOTTA: return Colors.WOOL_BROWN + shade;
-        // case GREEN_GLAZED_TERRACOTTA: return Colors.WOOL_GREEN + shade;
-        // case RED_GLAZED_TERRACOTTA: return Colors.WOOL_RED + shade;
-        // case BLACK_GLAZED_TERRACOTTA: return Colors.WOOL_BLACK + shade;
+        case WHITE_GLAZED_TERRACOTTA: return Colors.WOOL_WHITE + shade;
+        case ORANGE_GLAZED_TERRACOTTA: return Colors.WOOL_ORANGE + shade;
+        case MAGENTA_GLAZED_TERRACOTTA: return Colors.WOOL_MAGENTA + shade;
+        case LIGHT_BLUE_GLAZED_TERRACOTTA: return Colors.WOOL_LIGHT_BLUE + shade;
+        case YELLOW_GLAZED_TERRACOTTA: return Colors.WOOL_YELLOW + shade;
+        case LIME_GLAZED_TERRACOTTA: return Colors.WOOL_LIME + shade;
+        case PINK_GLAZED_TERRACOTTA: return Colors.WOOL_PINK + shade;
+        case GRAY_GLAZED_TERRACOTTA: return Colors.WOOL_GRAY + shade;
+        case SILVER_GLAZED_TERRACOTTA: return Colors.WOOL_SILVER + shade;
+        case CYAN_GLAZED_TERRACOTTA: return Colors.WOOL_CYAN + shade;
+        case PURPLE_GLAZED_TERRACOTTA: return Colors.WOOL_PURPLE + shade;
+        case BLUE_GLAZED_TERRACOTTA: return Colors.WOOL_BLUE + shade;
+        case BROWN_GLAZED_TERRACOTTA: return Colors.WOOL_BROWN + shade;
+        case GREEN_GLAZED_TERRACOTTA: return Colors.WOOL_GREEN + shade;
+        case RED_GLAZED_TERRACOTTA: return Colors.WOOL_RED + shade;
+        case BLACK_GLAZED_TERRACOTTA: return Colors.WOOL_BLACK + shade;
         case SUGAR_CANE_BLOCK: return Colors.LIGHT_GREEN + shade;
         case WATER_LILY: return Colors.DARK_GREEN + shade;
         case CACTUS: return Colors.DARK_GREEN + shade;
