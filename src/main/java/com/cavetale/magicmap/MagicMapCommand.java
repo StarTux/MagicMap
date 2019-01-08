2ff6d9e2aff46962fe05dfc42fc3f4238f638d95
StarTux <ths.loy@googlemail.com>
2019-01-08 14:53:11 +0100

    Give command

--- src/main/java/com/cavetale/magicmap/MagicMapCommand.java
+++ src/main/java/com/cavetale/magicmap/MagicMapCommand.java
@@ -1,6 +1,8 @@
 package com.cavetale.magicmap;
 
 import lombok.RequiredArgsConstructor;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -19,6 +21,9 @@ final class MagicMapCommand implements CommandExecutor {
             this.plugin.setupMap();
             this.plugin.importConfig();
             this.plugin.getMapGiver().reset();
+            for (Player target: this.plugin.getServer().getOnlinePlayers()) {
+                target.removeMetadata("magicmap.session", this.plugin);
+            }
             sender.sendMessage("MagicMap config reloaded");
             return true;
         }
@@ -30,6 +35,26 @@ final class MagicMapCommand implements CommandExecutor {
                 sender.sendMessage("Debug mode enabled");
             } else {
                 sender.sendMessage("Debug mode disabled");
+            }
+            return true;
+        }
+        case "give": {
+            if (args.length > 2) return false;
+            if (player == null && args.length < 2) return false;
+            Player target;
+            if (args.length >= 2) {
+                target = Bukkit.getPlayerExact(args[1]);
+                if (target == null) {
+                    sender.sendMessage(ChatColor.RED + "Player not found: " + args[1]);
+                    return true;
+                }
+            } else {
+                target = player;
+            }
+            if (target.getInventory().addItem(this.plugin.createMapItem()).isEmpty()) {
+                sender.sendMessage("MagicMap given to " + target.getName() + ".");
+            } else {
+                sender.sendMessage("Could not give MagicMap to " + target.getName() + ". Inventory is full.");
             }
             return true;
         }
