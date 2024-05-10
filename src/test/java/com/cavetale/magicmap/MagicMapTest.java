74c00afb32267e2567774afa4aa42eb57140a437
StarTux <ths.loy@gmail.com>
2024-05-10 15:21:54 +0200

    Skip the test until it is needed

--- src/test/java/com/cavetale/magicmap/MagicMapTest.java
+++ src/test/java/com/cavetale/magicmap/MagicMapTest.java
@@ -5,6 +5,6 @@ import org.junit.Test;
 public final class MagicMapTest {
     @Test
     public void test() throws Exception {
-        ColorGrabber.grabColors();
+        // ColorGrabber.grabColors();
     }
 }
